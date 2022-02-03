3b18f3feb2ee04b0f5c01994db2c65ffacae11cc
Katie Liu <kliu57@myseneca.ca>
2022-02-03 01:53:46 -0500

    added Workshop 3, P1 is done for 1st pass

---
+++ WS03/lab/Train.cpp
@@ -0,0 +1,83 @@
+/* ------------------------------------------------------
+Author	Katie Liu
+Email   kliu57@myseneca.ca
+ID      018889121
+Date    Thu Feb 3 2022
+
+Workshop 3 part 1
+Module: Train
+Desc:   Module for Train object and functions
+Filename: Train.cpp
+-----------------------------------------------------------*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+#include "Train.h"
+
+
+namespace sdds {
+
+	void Train::setSafeEmptyState() {
+		name[0] = '\0';						// set name member variable to empty string
+		numPeople = SAFE_EMPTY_NUM_PEOPLE;	// set number of people to invalid value
+		speed = SAFE_EMPTY_SPEED;			// set speed to invalid value
+	}
+
+	void Train::set(const char* inputName, int inputNumPeople, double inputSpeed) {
+		// validate input
+		bool nameIsValid = (inputName != nullptr && strlen(inputName) > 0) ? true : false;
+		bool numPeopleIsValid = (inputNumPeople >= 0 && inputNumPeople <= MAX_PEOPLE) ? true : false;
+		bool speedIsValid = (inputSpeed >= 0 && inputSpeed <= MAX_SPEED) ? true : false;
+
+		if (nameIsValid && numPeopleIsValid && speedIsValid) {
+			strcpy(name, inputName);	// input is validated, set member variables to input
+			numPeople = inputNumPeople;
+			speed = inputSpeed;
+		} else {
+			setSafeEmptyState();		// set member variables to safe empty state
+		}
+	}
+
+	int Train::getNumberOfPeople() const {
+		return numPeople;
+	}
+
+	const char* Train::getName() const {
+		return name;
+	}
+
+	double Train::getSpeed() const {
+		return speed;
+	}
+
+	bool Train::isSafeEmpty() const {
+		bool result = false;
+
+		// check if member variables are set to safe empty values
+		bool nameIsSafeEmpty = (name[0] == '\0') ? true : false;
+		bool numPeopleIsSafeEmpty = (numPeople == SAFE_EMPTY_NUM_PEOPLE) ? true : false;
+		bool speedIsSafeEmpty = (speed == SAFE_EMPTY_SPEED) ? true : false;
+
+		if (nameIsSafeEmpty && numPeopleIsSafeEmpty && speedIsSafeEmpty) {
+			result = true;	// if all member variables are safe empty return true
+		}
+
+		return result;
+	}
+
+	void Train::display() const {
+		int oldPrecision = cout.precision();
+		cout << "NAME OF THE TRAIN : " << name << endl;			// print name
+		cout << "NUMBER OF PEOPLE  : " << numPeople << endl;	// print number of people
+		cout << "SPEED             : ";
+		cout.setf(ios::fixed);						// set fixed format
+		cout.precision(DISPLAY_SPEED_PRECISION);	// change precision for speed display
+		cout << speed << " km/h" << endl;			// print speed
+		cout.unsetf(ios::fixed);					// unset fixed format
+		cout.precision(oldPrecision);				// change precision back
+	}
+}
---
+++ WS03/lab/Train.h
@@ -0,0 +1,59 @@
+/* ------------------------------------------------------
+Author	Katie Liu
+Email   kliu57@myseneca.ca
+ID      018889121
+Date    Thu Feb 3 2022
+
+Workshop 3 part 1
+Module: Train
+Desc:   Module for Train object and functions
+Filename: Train.h
+-----------------------------------------------------------*/
+
+#ifndef SDDS_TRAIN_H
+#define SDDS_TRAIN_H 
+
+namespace sdds {
+
+    const int MAX_NAME_LEN =20;
+    const int MAX_PEOPLE = 1000;
+    const int MAX_SPEED = 320;
+
+    const int SAFE_EMPTY_NUM_PEOPLE = -1;   // default value for number of people in safe empty state
+    const int SAFE_EMPTY_SPEED = -1;        // default value for speed in safe empty state
+    const int DISPLAY_SPEED_PRECISION = 2;  // how many decimals to display the speed at
+
+    class Train {
+        char name[MAX_NAME_LEN+1];
+        int numPeople;
+        double speed;
+
+        // set object to safe empty state using invalid values
+        // name is empty string, number of people and speed is -1
+        void setSafeEmptyState();
+
+    public:
+
+        // initialize a Train object
+        // receives the name of the train, the number of people on the train and the speed of the train
+        // validates the data values received and uses these data values to set the respective data members, only if all of them are valid
+        void set(const char*, int, double);
+
+        // returns number of people on train
+        int getNumberOfPeople() const;
+
+        // returns name of train
+        const char* getName() const;
+
+        // returns speed of train
+        double getSpeed() const;
+
+        // returns true if object is in a safe empty state; false otherwise
+        bool isSafeEmpty() const;
+
+        // prints formatted object info to standard output
+        void display() const;
+    };
+}
+
+#endif // ! SDDS_TRAIN_H
