863c5506b43d4c4bdbab0489237cf8978b21c3eb
Katie Liu <kliu57@myseneca.ca>
2022-04-09 21:25:25 -0400

    ms52 try 1

---
+++ m52/AidMan.cpp
@@ -0,0 +1,380 @@
+/* ------------------------------------------------------
+Final Project Milestone 5
+Module: AidMan
+Filename: AidMan.cpp
+Author	Katie Liu
+Email   kliu57@myseneca.ca
+ID      018889121
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#define  _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include <iomanip>
+#include "AidMan.h"
+#include "Utils.h"
+#include "Date.h"
+#include "Perishable.h"
+using namespace std;
+namespace sdds {
+	// private member functions
+
+	unsigned int AidMan::menu() const {
+		int year = 0;
+		int month = 0;
+		int day = 0;
+
+		ut.getSystemDate(&year, &month, &day);	// get date
+		Date currentDate(year, month, day);		// create date object
+
+		cout << "Aid Management System" << endl;
+		cout << "Date: ";
+		currentDate.write(cout) << endl;	// print formatted date
+
+		cout << "Data file: " << (m_filename != nullptr ? m_filename : "No file") << endl;
+		cout << "---------------------------------" << endl;
+
+		return m_mainMenu.run();	// run main menu
+	}
+
+	bool AidMan::validateFilename(const char* filename) const {
+		const int forbiddenSize = 10;
+		const char forbiddenChars[forbiddenSize] = "<>:\"/\\|?*";
+		bool filenameIsValid = true;
+		int filenameLen = 0;
+		
+		if (filename != nullptr) {
+			filenameLen = static_cast<int>(strlen(filename));
+			if (filenameLen) {
+				// set flag to false if any character is a forbidden character
+				for (int i = 0; i < filenameLen && filenameIsValid; i++) {	// go through every char in this string
+																			// go through all forbidden characters
+					for (int k = 0; k < forbiddenSize-1 && filenameIsValid; k++) {
+						if (filename[i] == forbiddenChars[k]) {
+							filenameIsValid = false;	// exit out of both loops
+						}
+					}
+				}
+			} else {
+				filenameIsValid = false;
+			}
+		}
+		return filenameIsValid;
+	}
+
+	void AidMan::save() const {
+		ofstream ofs;
+
+		if (m_filename != nullptr) {
+			// create/open file for writing
+			ofs.open(m_filename);
+
+			for (int i = 0; i < m_iProductNum; i++) {
+				m_iProducts[i]->save(ofs);
+			}
+
+			// close file
+			if (ofs.is_open()) {
+				ofs.close();
+			}
+		}
+	}
+
+	void AidMan::deallocate() {
+		// dealloc memory for each iProduct object
+		for (int i = 0; i < m_iProductNum; i++) {
+			delete m_iProducts[i];
+			m_iProducts[i] = nullptr;
+		}
+		m_iProductNum = 0;
+
+		delete [] m_filename;	// dealloc memory for filname char array
+		m_filename = nullptr;
+	}
+
+	// menu item 1
+	int AidMan::list(const char* sub_desc) {
+		int rowNum = 0;
+		int printedIndexes[sdds_max_num_items];	// stores indexes of all objects printed
+		int numPrinted = 0;
+		bool validInput = false;
+
+		// print list header
+		cout << " ROW |  SKU  | Description                         | Have | Need |  Price  | Expiry" << endl;
+		cout << "-----+-------+-------------------------------------+------+------+---------+-----------" << endl;
+
+		if (sub_desc == nullptr) {
+			// print all items
+			for (int i = 0; i < m_iProductNum; i++) {
+				cout << setw(4) << i+1 << " | ";		// print row number
+				m_iProducts[i]->linear(true);			// set to linear
+				m_iProducts[i]->display(cout) << endl;	// print item
+				printedIndexes[numPrinted++] = i;
+			}
+		} else {
+			// print only the items containing the sub_desc in their description
+			for (int i = 0; i < m_iProductNum; i++) {
+				if (*m_iProducts[i] == sub_desc) {
+					cout << setw(4) << i+1 << " | ";		// print row number
+					m_iProducts[i]->linear(true);			// set to linear
+					m_iProducts[i]->display(cout) << endl;	// print item
+					printedIndexes[numPrinted++] = i;
+				}
+			}
+		}
+
+		cout << "-----+-------+-------------------------------------+------+------+---------+-----------" << endl;
+
+		if (numPrinted) {
+			// prompt user to press <ENTER> or input an int between 1 and number of records printed
+			// validInput flag is true if they do one of the above
+			// if they make an invalid input, keep prompting them
+			while (!validInput) {
+				cout << "Enter row number to display details or <ENTER> to continue:\n> ";
+
+				if (cin.peek() != '\n') {
+					rowNum = ut.getint();
+
+					// check if user input number is a valid item in list
+					if (rowNum >= 1 && rowNum <= numPrinted) {
+						// user entered an int between 1 and num records printed
+						validInput = true;	// exit loop
+						// display the selected item in a non-linear format
+						m_iProducts[printedIndexes[rowNum-1]]->linear(false);
+						m_iProducts[printedIndexes[rowNum-1]]->display(cout) << endl;
+					}
+				} else {
+					// user pressed <ENTER>
+					validInput = true;	// exit loop
+				}
+			}
+		} else {
+			cout << "The list is emtpy!" << endl;
+		}
+
+		return numPrinted;
+	}
+
+	// menu item 2
+	void AidMan::addItem() {
+		int selection = 0;
+		int sku = 0;
+		iProduct* newProduct = nullptr;
+
+		// check if array of iProduct pointers is full
+		if (m_iProductNum < sdds_max_num_items) {
+			// prompt user to enter item type
+			selection = ut.getint(0, 2, "1- Perishable\n2- Non-Perishable\n-----------------\n0- Exit\n> ", "Invalid Integer");
+
+			if (selection) {
+				// create item based on user selection of type
+				if (selection == 1) {	// perishable
+					newProduct = new Perishable();
+				} else {	// non-perishable
+					newProduct = new Item();
+				}
+
+				sku = newProduct->readSku(cin);	// read sku from console
+
+				// check if sku is already present in database
+				if (search(sku) == -1) {	// sku not present in DB
+					// sku is not in the database, read the rest of the item details
+					newProduct->read(cin);
+
+					// check if read product is in good state
+					if (*newProduct) {
+						// add product pointer to array of pointers
+						m_iProducts[m_iProductNum++] = newProduct;
+					} else {
+						// display product and delete it
+						newProduct->display(cout);
+						delete newProduct;
+					}
+				} else {	// sku already present in DB
+					delete newProduct;	// delete new product since its a duplicate
+					cout << "Sku: " << sku << " is already in the system, try updating quantity instead.\n";
+				}
+			} else {
+				// user chooses to exit
+				cout << "Aborted\n";
+			}
+		} else {
+			cout << "Database full!\n";
+		}
+	}
+
+	// menu item 7
+	bool AidMan::load() {
+		ifstream ifs;
+		ofstream ofs;
+		int selection = 0;
+		char ch = 0;
+		bool filenameValid = false;
+
+		save();			// Saves all the already existing m_iProducts
+		deallocate();	// Deallocates all the resources of the AidMan class making it ready to load new information
+
+		// keep prompting for file name until a valid one is entered
+		while (!filenameValid) {
+			cout << "Enter file name: ";
+			ut.getcstring(m_filename);
+
+			// check that get filename worked
+			// if it did not work prompt user again
+			if (m_filename != nullptr) {
+				if (static_cast<int>(strlen(m_filename))) {
+					// trim leading and trailing spaces and set tabs, \n, to ' '
+					// filename set to nullptr if filename only has spaces
+					ut.formatCstring(m_filename);
+
+					if (m_filename != nullptr) {
+						// check filename for forbidden characters
+						filenameValid = validateFilename(m_filename);
+					}
+				}
+			}
+			if (!filenameValid) {
+				cout << "Invalid file name!" << endl;
+			}
+		}
+
+		// got filename, proceed to read from file and load items
+		ifs.open(m_filename);	// opens file for reading
+
+		if (!ifs.fail()) {
+			// go through each line in file and store each as an item/perishable
+			// continue until invalid record is found
+			while (!ifs.fail() && !ifs.eof()) {
+				// get first character
+				ch = ifs.peek();
+
+				if (isdigit(ch)) {
+					// check if iProduct array has room to load the item/perishable
+						if (ch == '1') {
+							// perishable
+							m_iProducts[m_iProductNum] = new Perishable();	// create a new Perishable item in the next available iProduct Pointers element
+						} else {
+							// item
+							m_iProducts[m_iProductNum] = new Item();		// create a new Item in the next available iProduct Pointers element
+						}
+
+						// load data from file stream into object
+						// since load function is virtual, dynamic binding will occur
+						m_iProducts[m_iProductNum]->load(ifs);
+
+						// check if item/perishable was loaded succesfully (in good state)
+						if (m_iProducts[m_iProductNum]) {
+							m_iProductNum++;	// increase iProduct counter
+						} else {
+							// item was not loaded sucessfully
+							delete m_iProducts[m_iProductNum];	// delete item
+							m_iProducts[m_iProductNum] = nullptr;
+						}
+				} else {
+					// invalid record found in file
+					ifs.setstate(ios::failbit);		// set stream to fail state, this quits the loop
+				}
+			}
+			
+			// print result from load
+			cout << m_iProductNum << " records loaded!" << endl;
+
+		} else {
+			// opening stream was unsuccessful
+			cout << "Failed to open " << m_filename << " for reading!" << endl;
+			selection = ut.getint(0, 1, "Would you like to create a new data file?\n1- Yes!\n0- Exit\n");
+			// create empty file
+			if (selection == 1) {
+				ofs.open(m_filename);
+				if (!ofs.is_open()) {
+					ofs.close();
+				}
+			}
+		}
+
+		// close stream
+		if (!ifs.is_open()) {
+			ifs.close();
+		}
+
+		// return true if num items loaded > 0
+		return m_iProductNum ? true : false;
+	}
+
+	int AidMan::search(int sku) const {
+		int indexOfMatch = -1;
+		for (int i = 0; i < m_iProductNum; i++) {
+			if (*m_iProducts[i] == sku) {	// check if item sku matches
+				indexOfMatch = i;
+			}
+		}
+		return indexOfMatch;
+	}
+
+	// public member functions
+
+	AidMan::AidMan() : m_mainMenu{NUM_MENU_OPTIONS, "1- List Items\n2- Add Item\n3- Remove Item\n4- Update Quantity\n5- Sort\n6- Ship Items\n7- New/Open Aid Database\n---------------------------------\n"} {
+		m_filename = nullptr;
+		m_iProductNum = 0;
+	}
+
+	AidMan::~AidMan() {
+		deallocate();
+	}
+
+	void AidMan::run() {
+		unsigned int userSelection = 0;
+
+		userSelection = menu();		// run menu and store user selection, selection will be from 0 to NUM_MENU_OPTIONS
+
+		while (userSelection != 0) {
+
+			// if selection is not 0 and filename is null change the selection to 7
+			if (m_filename == nullptr) {
+				userSelection = 7;
+			}
+
+			// user made a selection
+			// run the function associated with user selection
+
+			// print menu item that was just selected
+			cout << "\n****";
+			m_mainMenu.printMenuOptionNameByNumber(cout, userSelection) << "****" << endl;
+
+			switch (userSelection) {
+			case 1:		// 1- List Items
+				list(nullptr);
+				break;
+			case 2:		// 2- Add Item
+				addItem();
+				break;
+			case 3:		// 3- Remove Item
+				cout << "this is not implemented yet" << endl;
+				break;
+			case 4:		// 4- Update Quantity
+				cout << "this is not implemented yet" << endl;
+				break;
+			case 5:		// 5- Sort
+				cout << "this is not implemented yet" << endl;
+				break;
+			case 6:		// 6- Ship Items
+				cout << "this is not implemented yet" << endl;
+				break;
+			case 7:		// 7- New/Open Aid Database
+				load();
+				break;
+			default:
+				cout << "Unexpected error in run" << endl;
+				break;
+			}
+
+			cout << endl;
+
+			userSelection = menu();		// run menu and store user selection again
+		}
+
+		// the while loop ended so the user chose to quit
+		cout << "Exiting Program!" << endl;		// print ending message
+	}
+}
---
+++ m52/Utils.cpp
@@ -0,0 +1,349 @@
+/* ------------------------------------------------------
+Final Project Milestone 5
+Module: Utils
+Filename: Utils.cpp
+Author	Katie Liu
+Email   kliu57@myseneca.ca
+ID      018889121
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include <string>
+#include <ctime>
+#include <iomanip>
+#include "Utils.h"
+using namespace std;
+namespace sdds {
+    Utils ut;
+    void Utils::testMode(bool testmode) {
+        m_testMode = testmode;
+    }
+    void Utils::getSystemDate(int* year, int* mon, int* day) {
+        if (m_testMode) {
+            if(day) *day = sdds_testDay;
+            if(mon) *mon = sdds_testMon;
+            if(year) *year = sdds_testYear;
+        }
+        else {
+            time_t t = time(NULL);
+            tm lt = *localtime(&t);
+            if (day) *day = lt.tm_mday;
+            if (mon) *mon = lt.tm_mon + 1;
+            if (year) *year = lt.tm_year + 1900;
+        }
+    }
+    int Utils::daysOfMon(int month, int year)const {
+        int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, -1 };
+        int mon = (month >= 1 && month <= 12 ? month : 13)-1;
+        return days[mon] + int((mon == 1) * ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
+    }
+
+    void Utils::alocpy(char*& destination, const char* source) {
+        int sourceSize = 0;
+        int newDestSize = 0;
+        delete [] destination;   // release destination memory storage if any
+        destination = nullptr;
+        if (source != nullptr) {
+            sourceSize = static_cast<int>(strlen(source));
+            // only proceed to copy if source has content
+            if (sourceSize) {
+                newDestSize = sourceSize+1;
+                destination = new (nothrow) char[newDestSize];  // allocate new memory storage
+                if (destination != nullptr) {
+                    strcpy(destination, source);	// copy data to newly allocated memory location
+                } else {
+                    cout << "Unexpected error in alocpy" << endl;
+                }
+            }
+        }
+    }
+
+    void Utils::aloConcat(char*& destination, const char* source) {
+        char* newDest = nullptr;
+        int destSize = 0;
+        int newDestSize = 0;
+        int sourceSize = 0;
+
+        if (destination != nullptr) {
+            destSize = static_cast<int>(strlen(destination));
+
+            if (destSize) {
+                if (source != nullptr) {
+                    sourceSize = static_cast<int>(strlen(source));
+
+                    // only proceed if source has content
+                    if (sourceSize) {
+                        newDestSize = destSize + sourceSize + 1;
+                        newDest = new (nothrow) char[newDestSize];  // allocate new memory storage
+                        if (newDest != nullptr) {
+                            strcpy(newDest, destination);	// copy data to newly allocated memory location
+                            strcat(newDest, source);        // concatenate
+                            newDest[newDestSize-1] = '\0';
+
+                            delete [] destination;
+                            destination = newDest;  // set destination to point to new mem location
+                        } else {
+                            cout << "Unexpected error" << endl;
+                        }
+                    }
+                }
+            } else {
+                // destination is empty string so just directly copy
+                ut.alocpy(destination, source);
+            }
+        }
+    }
+
+    int Utils::getint(const char* prompt, istream& istr) {
+        int input = 0;
+        if (prompt != nullptr) {
+            cout << prompt;  // display prompt
+        }
+        istr >> input;    // read from user input, input stream object will be set to fail state if user input is not an integer
+        while (istr.fail()) {     // prompt user again until input stream object is not in fail state
+            cout << "Invalid Integer, retry: ";
+            istr.clear();
+            istr.ignore(1000, '\n');  // clear buffer
+            istr >> input;    // read from user input, input stream object will be set to fail state if user input is not an integer
+        }
+        return input;
+    }
+
+    int Utils::getint(int min, int max, const char* prompt, const char* errMes, istream& istr) {
+        int input = 0;
+        bool inRange = false;
+        if (prompt != nullptr) {
+            cout << prompt;  // display prompt
+        }
+        istr >> input;    // read from user input, input stream object will be set to fail state if user input is not an integer
+        if (!istr.fail()) {
+            if (input >= min && input <= max) {
+                inRange = true;
+            }
+        }
+        while (istr.fail() || !inRange) {     // prompt user again until input stream object is not in fail state
+                                              // display error message according to the situation
+            if (istr.fail()) {
+                if (errMes != nullptr) {
+                    cout << errMes << ", retry: ";
+                } else {
+                    cout << "Value out of range [" << min << "<=val<=" << max << "]: ";  // user did not provide an error message so print the default one
+                }
+            } else {
+                // assume !inRange
+                cout << "Value out of range [" << min << "<=val<=" << max << "]: ";
+            }
+
+            istr.clear();
+            istr.ignore(1000, '\n');  // clear buffer
+            istr >> input;    // read from user input, input stream object will be set to fail state if user input is not an integer
+            if (!istr.fail()) {
+                if (input >= min && input <= max) {
+                    inRange = true;
+                }
+            }
+        }
+
+        // clear buffer
+        istr.clear();
+        istr.ignore(1000, '\n');
+
+        return input;
+    }
+
+    double Utils::getdouble(double min, double max, const char* prompt, const char* errMes, istream& istr) {
+        double input = 0.0;
+        bool inRange = false;
+
+        // set precision
+        cout.setf(ios::fixed);
+        cout.precision(2);
+
+        if (prompt != nullptr) {
+            cout << prompt;  // display prompt
+        }
+        istr >> input;    // read from user input, input stream object will be set to fail state if user input is not an integer
+        if (!istr.fail()) {
+            if (input >= min && input <= max) {
+                inRange = true;
+            }
+        }
+        while (istr.fail() || !inRange) {     // prompt user again until input stream object is not in fail state
+                                              // display error message according to the situation
+            if (istr.fail()) {
+                if (errMes != nullptr) {
+                    cout << errMes << ", retry: ";
+                } else {
+                    cout << "Value out of range [" << min << "<=val<=" << max << "]: ";  // user did not provide an error message so print the default one
+                }
+            } else {
+                // assume !inRange
+                cout << "Value out of range [" << min << "<=val<=" << max << "]: ";
+            }
+
+            istr.clear();
+            istr.ignore(1000, '\n');  // clear buffer
+            istr >> input;    // read from user input, input stream object will be set to fail state if user input is not an integer
+            if (!istr.fail()) {
+                if (input >= min && input <= max) {
+                    inRange = true;
+                }
+            }
+        }
+
+        // clear buffer
+        istr.clear();
+        istr.ignore(1000, '\n');
+
+        // set precision back
+        cout.unsetf(ios::fixed);
+        cout.precision(6);
+
+        return input;
+    }
+
+    bool Utils::findInCstring(const char* cstring, const char* toFind) {
+        bool found = false;
+        const char* chPtr = strstr(cstring, toFind);
+        if (chPtr != nullptr) {
+            found = true;
+        }
+        return found;
+    }
+
+    void Utils::getFileCstring(char*& dest, ifstream& ifstr, char delim) {
+        const int maxSize = 200;        // this is how many characters we will attempt to read from the file before stopping
+                                        // if delimiter is not reached, we will dynamically allocate memory and read further
+        char fixedSizeString[maxSize];
+        char ch = 0;
+        int i = 0;
+        bool hitDelim = false;
+
+        ifstr.get(ch); 
+        if (ch == delim) {
+            hitDelim = true;
+        }
+
+        // read char by char until hitting delim or maxSize
+        for (i = 0; i < maxSize-1 && !ifstr.eof() && !hitDelim; i++) {
+            fixedSizeString[i] = ch;
+
+            // read the cString stopping at the size limit
+            if (i < maxSize-2) {
+                ifstr.get(ch); 
+
+                if (ch == delim) {
+                    hitDelim = true;
+                }
+            }
+        }   
+        fixedSizeString[i] = '\0'; // make sure the cString is null terminated
+
+        ut.alocpy(dest, fixedSizeString);    // set string as final
+
+        // continue reading if still havent hit delimiter
+        while (!hitDelim && !ifstr.eof()) {
+
+            hitDelim = false;
+            ifstr.get(ch); 
+            if (ch == delim) {
+                hitDelim = true;
+            }
+            // read char by char until hitting delim or maxSize
+            for (i = 0; i < maxSize-1 && !ifstr.eof() && !hitDelim; i++) {
+                fixedSizeString[i] = ch;
+
+                // read the cString stopping at the size limit
+                if (i < maxSize-2) {
+                    ifstr.get(ch); 
+
+                    if (ch == delim) {
+                        hitDelim = true;
+                    }
+                }
+            }   
+            fixedSizeString[i] = '\0'; // make sure the fixedSizeString is null terminated
+            
+            aloConcat(dest, fixedSizeString);    // concat and set as final
+        }
+
+        // if reached the end and delimiter not found, set stream to fail state
+        if (!hitDelim) {
+            ifstr.setstate(ios::failbit);
+        }
+    }
+
+    void Utils::formatCstring(char*& cstring) {
+        int i = 0;
+        int consecutiveSpaces = 0;
+        int size = 0;
+        char ch = 0;
+        char* formatted = nullptr;
+        int formattedIndex = 0;
+
+        if (cstring != nullptr) {
+            size = static_cast<int>(strlen(cstring) + 1);
+
+            if (size) {
+                formatted = new (nothrow) char[size];  // dynamically make a copy of the cstring to store the formatted version
+
+                if (formatted != nullptr) {
+                    // skip leading whitespace
+                    for (i = 0; i < size-1 && isspace(cstring[i]); i++) {
+                    }
+                    // now i is the index of the first nonspace character
+
+                    // go through the rest of the characters and add them to formatted, changing tabs and newlines to spaces
+                    for (i = i; i < size-1; i++) {
+                        ch = cstring[i];
+
+                        if (isspace(ch)) {
+                            formatted[formattedIndex++] = ' ';
+                            consecutiveSpaces++;
+                        } else {
+                            formatted[formattedIndex++] = ch;
+                            consecutiveSpaces = 0;
+                        }
+                    }
+
+                    // if there are consectutive spaces at the end, remove them
+                    if (consecutiveSpaces) {
+                        formatted[size - consecutiveSpaces - 1] = '\0';
+                    } else {
+                        formatted[i] = '\0';
+                    }
+
+                    if (static_cast<int>(strlen(formatted))) {
+                        // safely copy string
+                        ut.alocpy(cstring, formatted);
+                    } else {
+                        // formatted string is empty so dealloc and set string to nullptr
+                        delete [] cstring;
+                        cstring = nullptr;
+                    }
+                }
+            } else {
+                // string is already empty string from the start, dealloc and set to nullptr
+                delete [] cstring;
+                cstring = nullptr;
+            }
+        }
+        delete [] formatted;    // dealloc temp memory
+    }
+
+    void Utils::getcstring(char*& dest, istream& istr) {
+        string inputString;
+
+        getline(istr, inputString); // get user input from console
+
+        if (inputString.length()) {
+            ut.alocpy(dest, inputString.c_str());
+        } else {
+            delete [] dest;
+            dest = nullptr;
+        }
+    }
+}
