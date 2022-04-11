522e4c130b5174e6300ac05de1cd20f41dff59d1
Katie Liu <kliu57@myseneca.ca>
2022-04-10 22:50:26 -0400

    final

---
+++ m56/AidMan.h
@@ -0,0 +1,94 @@
+/* ------------------------------------------------------
+Final Project Milestone 5
+Module: AidMan
+Filename: AidMan.h
+Author	Katie Liu
+Email   kliu57@myseneca.ca
+ID      018889121
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#ifndef SDDS_AIDMAN_H
+#define SDDS_AIDMAN_H
+#include <iostream>
+#include "Menu.h"
+#include "iProduct.h"
+namespace sdds {
+    const int NUM_MENU_OPTIONS = 7;
+    const int sdds_max_num_items = 100;
+    class AidMan {
+        char* m_filename{};
+        Menu m_mainMenu;
+        iProduct* m_iProducts[sdds_max_num_items]{};    // array of iProduct pointers
+        int m_iProductNum;
+
+        // Returns an unsigned integer that is the user's selection of an option in the main menu of the system
+        unsigned int menu() const;
+
+        // Returns true if filename does not contain forbidden characters
+		bool validateFilename(const char* filename) const;
+
+        // if the filename attribute is not null
+        // Creates an ofstream object using the filename (to write into)
+        // then function goes through the iProduct Pointers up to the number of number of iProduct Items and calls the save() of each iProduct to write them in the file.
+        // if the filename attribute is null, this function does nothing.
+        void save() const;
+
+        // deletes all the dynamic memory allocated in iProduct Pointers elements and the filename, and then sets number of iProduct Items to zero.
+        void deallocate();
+
+        // menu item 1
+        // prints all items, asks user if they want to see more details
+        // returns number of items printed
+        int list(const char* sub_desc = nullptr);
+
+        // menu item 2
+        // prompt user to choose perishable or non-perishable, then a Perishable or Non-Perishable Item is allocated in an iProduct pointer
+        // prompt user for SKU, if SKU not found in existing items, the rest of the data is read from the console
+        // If the read iProduct is in a good state, it is added to the next available element of the iProduct Pointers array and number of iProduct Items is added by one
+        // otherwise, the allocated Item is displayed and then deleted.
+        void addItem();
+
+        // menu item 3
+        // prompts for sku and removes the item from database if found
+        void removeItem();
+
+        // menu item 4
+        // update qty of item
+        void updateQuantity();
+
+        // menu item 5
+        void sort();
+
+        // menu item 6
+        void shipItems();
+
+        // menu item 7
+        // Loads data records from a data file and returns true if at least one record is loaded
+        bool load();
+
+        // Loops through all the iProduct Pointers elements and if the SKU is a match it will return the index, otherwise it will return -1
+        int search(int sku) const;
+
+        // deletes the iProduct at index in iProduct Pointers.
+        // Then shifts all the iProducts to left once and reduces number of iProduct Items by one
+        void remove(int index);
+
+    public:
+        // constructor
+        AidMan();
+
+        // prohibit copying and copy assigning
+        AidMan(const AidMan& source) = delete;
+        AidMan& operator=(const AidMan& source) = delete; 
+
+        // destructor
+        ~AidMan();
+
+        // Display menu and prompt for user to enter an integer between 0 and m_options
+        // Continue to prompt the user until a valid input is received and return that integer
+        void run();
+    };
+}
+#endif // !SDDS_AIDMAN_H
---
+++ m56/Status.cpp
@@ -0,0 +1,96 @@
+/* ------------------------------------------------------
+Final Project Milestone 5
+Module: Status
+Filestatus: Status.cpp
+Author	Katie Liu
+Email   kliu57@myseneca.ca
+ID      018889121
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code 
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#define  _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include "Status.h"
+#include "Utils.h"
+using namespace std;
+namespace sdds {
+	// public member functions
+
+	Status::Status(const char* desc) {
+		ut.alocpy(m_desc, desc);	// safely alloc dynamic memory - copy desc of argument object to m_desc
+		m_code = 0;
+	}
+
+	Status::~Status() {
+		delete [] m_desc;
+	}
+
+	// copy constructor
+	Status::Status(const Status& statusObject) {
+		*this = statusObject;
+	}
+
+	Status& Status::operator=(const Status& statusObject) {
+		if (this != &statusObject) {	// check that addresses are not the same
+			ut.alocpy(m_desc, statusObject.m_desc);		// safely alloc dynamic memory - copy desc of argument object to m_desc
+			m_code = statusObject.m_code;				// shallow copy of code of argument object to m_code
+		} else {
+			clear();
+		}
+		return *this;
+	}
+
+	Status& Status::operator=(const char* desc) {
+		if (desc != nullptr) {
+			if (&m_desc != &desc) {	// check that addresses are not the same and arg object is valid
+				ut.alocpy(m_desc, desc);		// safely alloc dynamic memory - copy desc of argument object to m_desc
+			} else {
+				clear();
+			}
+		} else {
+			clear();
+		}
+		return *this;
+	}
+
+	Status& Status::operator=(int code) {
+		m_code = code;
+		return *this;
+	}
+
+	Status::operator const char*() const {
+		return m_desc;
+	}
+
+	Status::operator int() const {
+		return m_code;
+	}
+
+	Status::operator bool() const {
+		return (m_desc == nullptr) ? true : false;
+	}
+
+	Status& Status::clear() {
+		delete [] m_desc;
+		m_desc = nullptr;
+		m_code = 0;
+		return *this;
+	}
+
+	ostream& Status::print(ostream& ostr) const {
+		if (!*this) {	// only print if current object is invalid
+			if (m_code != 0) {	// only print error code if not zero
+				ostr << "ERR#" << m_code << ": ";
+			}
+			ostr << m_desc;
+		}
+		return ostr;
+	}
+
+	// helper functions
+
+	ostream& operator<<(ostream& ostr, const Status& statusObject) {
+		return statusObject.print(ostr);
+	}
+}
